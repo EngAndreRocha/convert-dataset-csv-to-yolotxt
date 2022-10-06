42f63cab9ba2cca9055635563988e77d481b2a8f
Andre Correia Rocha <rochandre@gmail.com>
2022-10-06 20:50:45 +0200

    added basic ifstream and uploaded csv files to directory

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include "hello.hpp"
 
-int main(int argc, char** argv)
+#define CSV_FILENAME "BBox_List_2017.csv"
+
+int main(int argc, char **argv)
 {
     hello::say_hello();
+    std::fstream ifs;
+    ifs.open(CSV_FILENAME, std::fstream::in);
+
+    char c = ifs.get();
+
+    while (ifs.good())
+    {
+        std::cout << c;
+        c = ifs.get();
+    }
+
+    ifs.close();
     return 0;
-}    //end of main function code block
+} // end of main function code block
